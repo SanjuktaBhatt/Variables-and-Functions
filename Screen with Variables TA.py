55840b5cc3d6eaaa872f6d5d8ebfb5230d419be5
SanjuktaBhatt <64887786+SanjuktaBhatt@users.noreply.github.com>
2021-05-26 13:01:13 +0530

    Add files via upload

---
+++ Screen with Variables TA.py
@@ -0,0 +1,29 @@
+import pygame
+
+pygame.init() 
+WHITE = (255,255,255)
+DARKBLUE = (36,90,190)
+LIGHTBLUE = (0,176,240)
+RED = (255,0,0)
+ORANGE = (255,100,0)
+YELLOW = (255,255,0)
+
+
+#Declare a variable "score" and assign it the value 0
+
+
+
+size = (600, 600)
+screen = pygame.display.set_mode(size)
+pygame.display.set_caption("Breakout Game")
+carryOn = True
+while carryOn:
+    for event in pygame.event.get(): # User did something
+            if event.type == pygame.QUIT: # If user clicked close
+                  carryOn = False # Flag that we are done so we exit this loop             
+    screen.fill(DARKBLUE)
+    font = pygame.font.Font(None, 34)
+    text = font.render("Score: " + str(score), 1, WHITE)
+    screen.blit(text, (20,10))
+    
+    pygame.display.flip()
