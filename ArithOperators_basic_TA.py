bb17f377f35fcb91edaa28ef580d174f2d35c376
SanjuktaBhatt <64887786+SanjuktaBhatt@users.noreply.github.com>
2021-05-26 13:47:40 +0530

    Add files via upload

---
+++ ArithOperators_basic_TA.py
@@ -0,0 +1,7 @@
+print(90+4)#adds two numbers
+print(90-4)#subtract two numbers
+print(90*4)#multiply two numbers
+print(90/4)#divide two numbers
+print(90%4)#remainder on division of two numbers
+print(90//4)#quotient on division of two numbers
+print(90**4)#raising one number to the power two numbers
