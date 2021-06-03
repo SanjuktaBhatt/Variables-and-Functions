a43e215884ad1848df7c145964a36c9cb7b07ca2
SanjuktaBhatt <64887786+SanjuktaBhatt@users.noreply.github.com>
2021-06-03 18:06:37 +0530

    Create G11_c1_TA.py

---
+++ G11_c1_TA.py
@@ -0,0 +1,24 @@
+import pygame
+pygame.init() 
+WHITE = (255,255,255)
+DARKBLUE = (36,90,190)
+LIGHTBLUE = (0,176,240)
+RED = (255,0,0)
+ORANGE = (255,100,0)
+YELLOW = (255,255,0)
+size = (600, 600)
+screen = pygame.display.set_mode(size)
+pygame.display.set_caption("Breakout Game")
+
+carryOn = True
+while carryOn:
+    for event in pygame.event.get(): # User did something
+            if event.type == pygame.QUIT: # If user clicked close
+                  carryOn = False # Flag that we are done so we exit this loop             
+    screen.fill(DARKBLUE)
+    #Add paddle here
+    
+    
+    pygame.display.flip()
+pygame.quit()
+    
